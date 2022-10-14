db5bd542c0654333362e932a3a129e175a834eb0
zhoujiahe <8519554+zhou-jiahe@user.noreply.gitee.com>
2022-10-14 15:41:32 +0800

    add tailwindcss

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-yarn dev 同时启动mock服务器
+```
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+```
+
+```js
+// tailwind.config.cjs
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+```css
+// index.css (引入全局文件)
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+```
+
+```
+npm run dev
+```
+
+
--- src/components/HelloWorld/index.tsx
+++ src/components/HelloWorld/index.tsx
@@ -3,18 +3,21 @@ import { Button, Card } from 'antd';
 import './style.scss';
 
 function App() {
-  const [count, setCount] = useState<number>(0);
+	const [count, setCount] = useState<number>(0);
 
-  return (
-    <div className='hello'>
-      <h1 className='title'>Vite + React</h1>
-      <Card title='按需引入antd'>
-        <Button type='primary' onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-      </Card>
-    </div>
-  );
+	return (
+		<div className='hello'>
+			<h1 className='title'>Vite + React</h1>
+			<Card title='按需引入antd'>
+				<Button type='primary' onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</Button>
+			</Card>
+			<div className="bg-red-500 hover:bg-blue-700">
+				Hover me
+			</div>
+		</div>
+	);
 }
 
 export default App;
